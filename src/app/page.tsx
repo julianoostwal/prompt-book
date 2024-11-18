fd02ce0dc828b82b303e032dbfc5fd5e002dad94
Julian Oostwal <info@julianoostwal.dev>
2024-11-18 14:59:59 +0100

    ss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ import {
   ModalFooter,
   Input,
   Textarea,
+  useDisclosure,
 } from "@nextui-org/react";
 import { motion } from "framer-motion";
 
-// Basis URL van je API
 const API_BASE_URL = "http://5.253.247.243:8000";
 
 export default function Home() {
@@ -23,62 +23,54 @@ export default function Home() {
   >([]);
   const [tags, setTags] = useState<{ id: number; name: string }[]>([]);
   const [error, setError] = useState<string | null>(null);
-  const [modalOpen, setModalOpen] = useState(false);
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
   const [newPrompt, setNewPrompt] = useState({
     content: "",
     description: "",
   });
 
-  // Functie om prompts op te halen
   const fetchPrompts = async () => {
     try {
       const response = await axios.get(`${API_BASE_URL}/prompt_fragments`);
       setPrompts(response.data);
-    } catch (err) {
+    } catch {
       setError("Er is een probleem met het ophalen van de prompts.");
     }
   };
 
-  // Functie om tags op te halen
   const fetchTags = async () => {
     try {
       const response = await axios.get(`${API_BASE_URL}/tags`);
       setTags(response.data);
-    } catch (err) {
+    } catch {
       setError("Er is een probleem met het ophalen van de tags.");
     }
   };
 
-  // Voeg een nieuwe prompt toe
   const handleAddPrompt = async () => {
     try {
       const response = await axios.post(`${API_BASE_URL}/prompt_fragments`, {
         content: newPrompt.content,
         description: newPrompt.description,
+        author_id: 1,
       });
       setPrompts([...prompts, response.data]);
       setNewPrompt({ content: "", description: "" });
-      handleModalClose();
-    } catch (err) {
+      onClose();
+    } catch {
       setError("Er is een probleem met het toevoegen van de prompt.");
     }
   };
 
-  // Verwijder een prompt
   const handleDeletePrompt = async (id: number) => {
     try {
       await axios.delete(`${API_BASE_URL}/prompt_fragments/${id}`);
       setPrompts(prompts.filter((prompt) => prompt.id !== id));
-    } catch (err) {
+    } catch {
       setError("Er is een probleem met het verwijderen van de prompt.");
     }
   };
 
-  // Sluit de modal
-  const handleModalClose = () => setModalOpen(false);
-  const handleModalOpen = () => setModalOpen(true);
-
-  // Ophalen van prompts en tags bij het laden van de pagina
   useEffect(() => {
     fetchPrompts();
     fetchTags();
@@ -98,12 +90,32 @@ export default function Home() {
         )}
 
         <section className="mb-6">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-            Beschikbare ChatGPT Prompts
-          </h2>
+          <div className="flex justify-between flex-wrap gap-4">
+            <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+              Beschikbare ChatGPT Prompts
+            </h2>
+            <Button color="primary" onPress={onOpen}>
+              Voeg nieuwe prompt toe
+            </Button>
+          </div>
+
+
+          <section className="mb-6 flex">
+            <h2 className="text-2xl font-semibold text-gray-800 mb-4 mr-6">Tags</h2>
+            <div className="flex flex-wrap gap-4">
+              {tags.map((tag) => (
+                <div
+                  key={tag.id}
+                  className="bg-blue-100 text-blue-600 px-4 py-2 rounded-md"
+                >
+                  {tag.name}
+                </div>
+              ))}
+            </div>
+          </section>
 
           <motion.div
-            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
+            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-4"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 1 }}
@@ -111,75 +123,63 @@ export default function Home() {
             {prompts.map((prompt) => (
               <motion.div
                 key={prompt.id}
-                className="bg-white p-4 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300"
+                className="bg-white p-4 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 flex flex-col justify-between h-full"
                 whileHover={{ scale: 1.05 }}
                 transition={{ type: "spring", stiffness: 300 }}
               >
-                <h3 className="text-xl font-semibold text-blue-500">
-                  {prompt.description}
-                </h3>
+              <div>
+                <h3 className="text-xl font-semibold text-blue-500">{prompt.description}</h3>
                 <p className="text-gray-700 mt-2">{prompt.content}</p>
-                <Button
-                  size="sm"
-                  color="warning"
-                  className="mt-4"
-                  onPress={() => handleDeletePrompt(prompt.id)}
-                >
-                  Verwijderen
-                </Button>
-              </motion.div>
+              </div>
+              <Button
+                size="sm"
+                color="danger"
+                className="mt-4"
+                onPress={() => handleDeletePrompt(prompt.id)}
+              >
+                Verwijderen
+              </Button>
+            </motion.div>            
             ))}
           </motion.div>
         </section>
 
-        <section className="mb-6">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">Tags</h2>
-          <div className="flex flex-wrap gap-4">
-            {tags.map((tag) => (
-              <div
-                key={tag.id}
-                className="bg-blue-100 text-blue-600 px-4 py-2 rounded-md"
-              >
-                {tag.name}
-              </div>
-            ))}
-          </div>
-        </section>
-
-        <Button color="primary" onPress={handleModalOpen}>
-          Voeg nieuwe prompt toe
-        </Button>
-
-        {/* Modal voor nieuwe prompt toevoegen */}
-        <Modal isOpen={modalOpen} onClose={handleModalClose}>
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
           <ModalContent>
-            <ModalHeader>Nieuwe Prompt Toevoegen</ModalHeader>
-            <ModalBody>
-              <Input
-                isClearable
-                placeholder="Beschrijving"
-                value={newPrompt.description}
-                onChange={(e) =>
-                  setNewPrompt({ ...newPrompt, description: e.target.value })
-                }
-              />
-              <Textarea
-                placeholder="Inhoud"
-                value={newPrompt.content}
-                onChange={(e) =>
-                  setNewPrompt({ ...newPrompt, content: e.target.value })
-                }
-                className="mt-4"
-              />
-            </ModalBody>
-            <ModalFooter>
-              <Button color="warning" onPress={handleModalClose}>
-                Annuleren
-              </Button>
-              <Button color="primary" onPress={handleAddPrompt}>
-                Toevoegen
-              </Button>
-            </ModalFooter>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  Maak nieuwe prompt aan
+                </ModalHeader>
+                <ModalBody>
+                  <Input
+                    label="Description"
+                    value={newPrompt.description}
+                    onChange={(e) =>
+                      setNewPrompt({
+                        ...newPrompt,
+                        description: e.target.value,
+                      })
+                    }
+                  />
+                  <Textarea
+                    label="Content"
+                    value={newPrompt.content}
+                    onChange={(e) =>
+                      setNewPrompt({ ...newPrompt, content: e.target.value })
+                    }
+                  />
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="danger" variant="light" onPress={onClose}>
+                    Sluiten
+                  </Button>
+                  <Button color="primary" onPress={handleAddPrompt}>
+                    Opslaan
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
           </ModalContent>
         </Modal>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 export default {
   content: [
@@ -7,13 +8,6 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 } satisfies Config;
