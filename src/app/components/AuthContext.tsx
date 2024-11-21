0073697378171451997f5fd6277799b56f8aba74
Julian Oostwal <info@julianoostwal.dev>
2024-11-21 13:34:11 +0100

    d

--- src/app/components/AuthContext.tsx
+++ src/app/components/AuthContext.tsx
@@ -7,19 +7,15 @@ type AuthContextType = {
   logout: () => void;
 };
 
-// Create an authentication context with an initial value of `null` as default
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 type AuthProviderProps = {
   children: ReactNode;
 };
 
-// Create a provider component
 export function AuthProvider({ children }: AuthProviderProps) {
-  // Initial state gets the token from local storage if it exists
   const [token, setToken] = useState<string | null>(null);
 
-  // Check localStorage for token on initial load (if any)
   useEffect(() => {
     const savedToken = localStorage.getItem('token');
     if (savedToken) {
@@ -27,13 +23,11 @@ export function AuthProvider({ children }: AuthProviderProps) {
     }
   }, []);
 
-  // Login function to set token
   const login = (newToken: string) => {
     localStorage.setItem('token', newToken);
     setToken(newToken);
   };
 
-  // Logout function to remove token
   const logout = () => {
     localStorage.removeItem('token');
     setToken(null);
@@ -46,7 +40,6 @@ export function AuthProvider({ children }: AuthProviderProps) {
   );
 }
 
-// Custom hook to use the AuthContext
 export function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined) {
